9e9143a95d9186f7f9713cd696b06194ab04b00f
truls27a <truls369@gmail.com>
2025-06-29 07:01:46 +0200

    refactor: create helper functions

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
-use std::fs::File;
-use std::io::prelude::*;
-use std::io::BufReader;
 use std::env;
+use std::fs::File;
+use std::io::BufReader;
+use std::io::Lines;
+use std::io::prelude::*;
 
 struct GrepConfig {
     query: String,
@@ -11,33 +12,29 @@ struct GrepConfig {
 impl GrepConfig {
     fn new(env_args: &Vec<String>) -> Result<GrepConfig, &str> {
         if env_args.len() < 2 {
-            return Err("Query and file path args missing!")
+            return Err("Query and file path args missing!");
         } else if env_args.len() < 3 {
-            return Err("File path arg missing!")
+            return Err("File path arg missing!");
         }
 
         return Ok(GrepConfig {
             query: env_args[1].to_string(),
-            file_path: env_args[2].to_string()
-        })
+            file_path: env_args[2].to_string(),
+        });
     }
 }
 
-fn main() -> std::io::Result<()> {
-    let env_args: Vec<String> = env::args().collect();
-    let grep_config = match GrepConfig::new(&env_args) {
-        Ok(grep_config) => grep_config,
-        Err(message) => panic!("{message}")
-    };
-    
+fn parse_lines(file_path: &str) -> Result<Lines<BufReader<File>>, std::io::Error> {
+    let file: File = File::open(file_path)?;
+    let buff_reader: BufReader<File> = BufReader::new(file);
+    let lines: std::io::Lines<BufReader<File>> = buff_reader.lines();
+    return Ok(lines);
+}
 
+fn query_lines(lines: Lines<BufReader<File>>, query: &str) -> Vec<String> {
+    let mut matching_lines: Vec<String> = Vec::new();
 
-    let file = File::open(grep_config.file_path)?;
-    let buff_reader = BufReader::new(file);
-
-    let mut matching_lines = Vec::new();
-
-    for line_result in buff_reader.lines() {
+    for line_result in lines {
         let line = match line_result {
             Ok(line) => line,
             Err(_) => {
@@ -46,12 +43,28 @@ fn main() -> std::io::Result<()> {
             }
         };
 
-        if line.contains(&grep_config.query) {
+        if line.contains(query) {
             matching_lines.push(line)
         }
-    }
+    };
+
+    return matching_lines
+}
+
+fn main() {
+    let env_args: Vec<String> = env::args().collect();
+    let grep_config: GrepConfig = match GrepConfig::new(&env_args) {
+        Ok(grep_config) => grep_config,
+        Err(message) => panic!("{message}"),
+    };
+
+    let lines = match parse_lines(&grep_config.file_path) {
+        Ok(lines) => lines,
+        Err(_) => panic!("Failed to read lines"),
+    };
+
+    let matching_lines: Vec<String> = query_lines(lines, &grep_config.query);
 
     println!("{:?}", matching_lines);
 
-    Ok(())
 }
