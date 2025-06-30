327dfcce51e086b17bcf8f730119637ef9778afa
truls27a <truls369@gmail.com>
2025-06-30 13:38:56 +0200

    refactor: create highlight implementation for Line

--- src/lib.rs
+++ src/lib.rs
@@ -55,7 +55,7 @@ impl Config {
 }
 
 #[derive(Debug, PartialEq)]
-pub struct Line {
+struct Line {
     index: usize,
     content: String,
 }
@@ -64,9 +64,39 @@ impl Line {
     fn new(index: usize, content: String) -> Line {
         Line { index, content }
     }
+
+    fn highlight(&self, query: &str) -> String {
+        let mut colored_content = String::new();
+
+        let content_chars: Vec<char> = self.content.chars().collect();
+        let mut content_index = 0;
+
+        let query_chars: Vec<char> = query.chars().collect();
+        let mut matching_query_index = 0;
+
+        while content_index < content_chars.len() {
+            colored_content.push(content_chars[content_index]);
+
+            if content_chars[content_index] == query_chars[matching_query_index] {
+                matching_query_index += 1;
+            } else {
+                matching_query_index = 0;
+            }
+
+            if matching_query_index == query_chars.len() {
+                colored_content.insert_str(content_index - matching_query_index + 1, "\x1b[31m");
+                colored_content.push_str("\x1b[0m");
+                matching_query_index = 0;
+            }
+
+            content_index += 1;
+        }
+
+        colored_content
+    }
 }
 
-pub fn search<'a>(
+fn search<'a>(
     query: &str,
     contents: &'a str,
     ignore_case: bool,
@@ -145,35 +175,7 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     for line in lines {
         let colored_content = match config.inverted_match {
             true => line.content,
-            false => {
-                let mut colored_content = String::new();
-
-                let content_chars: Vec<char> = line.content.chars().collect();
-                let mut content_index = 0;
-
-                let query_chars: Vec<char> = config.query.chars().collect();
-                let mut matching_query_index = 0;
-
-                while content_index < content_chars.len() {
-                    colored_content.push(content_chars[content_index]);
-
-                    if content_chars[content_index] == query_chars[matching_query_index] {
-                        matching_query_index += 1;
-                    } else {
-                        matching_query_index = 0;
-                    }
-
-                    if matching_query_index == query_chars.len() {
-                        colored_content.insert_str(content_index-matching_query_index+1, "\x1b[31m");
-                        colored_content.push_str("\x1b[0m");
-                        matching_query_index = 0;
-                    }
-
-                    content_index += 1;
-                }
-
-                colored_content
-            }
+            false => line.highlight(&config.query),
         };
 
         if config.show_line_numbers {
