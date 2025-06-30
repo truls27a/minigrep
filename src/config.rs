193037dbc6de608175c3a9c8809241d2707fd270
truls27a <truls369@gmail.com>
2025-06-30 15:53:05 +0200

    feat(tests): add tests to config

--- src/config.rs
+++ src/config.rs
@@ -72,3 +72,101 @@ impl Config {
     }
 }
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn config_creation() {
+        let query = String::from("Rust!");
+        let file_path = String::from("src/main.rs");
+        let ignore_case = false;
+        let show_line_numbers = true;
+        let only_match_words = true;
+        let inverted_match = false;
+
+        let config = Config::new(
+            query.clone(),
+            file_path.clone(),
+            ignore_case,
+            show_line_numbers,
+            only_match_words,
+            inverted_match,
+        );
+
+        assert_eq!(config.query, query);
+        assert_eq!(config.file_path, file_path);
+        assert_eq!(config.ignore_case, ignore_case);
+        assert_eq!(config.show_line_numbers, show_line_numbers);
+        assert_eq!(config.only_match_words, only_match_words);
+        assert_eq!(config.inverted_match, inverted_match);
+    }
+
+    #[test]
+    fn config_build() {
+        let query = String::from("To be, or not to be");
+        let file_path = String::from("poem.txt");
+        let ignore_case = true;
+        let show_line_numbers = true;
+        let only_match_words = false;
+        let inverted_match = true;
+
+        let mut args: Vec<String> = vec!["src/main.rs".into()];
+
+        if ignore_case {
+            args.push(String::from("-i"));
+        }
+        if show_line_numbers {
+            args.push(String::from("-n"));
+        }
+        if only_match_words {
+            args.push(String::from("-w"));
+        }
+        if inverted_match {
+            args.push(String::from("-v"));
+        }
+        
+        args.push(query.clone()); // Needs to be second last
+        args.push(file_path.clone()); // Needs to be last
+
+        let config = Config::build(&args).unwrap();
+
+        assert_eq!(config.query, query);
+        assert_eq!(config.file_path, file_path);
+        assert_eq!(config.ignore_case, ignore_case);
+        assert_eq!(config.show_line_numbers, show_line_numbers);
+        assert_eq!(config.only_match_words, only_match_words);
+        assert_eq!(config.inverted_match, inverted_match);
+    }
+
+    #[test]
+    fn config_failed_build() {
+        let file_path = String::from("poem.txt");
+        let ignore_case = true;
+        let show_line_numbers = true;
+        let only_match_words = false;
+        let inverted_match = true;
+
+        let mut args: Vec<String> = vec!["src/main.rs".into()];
+
+        if ignore_case {
+            args.push(String::from("-i"));
+        }
+        if show_line_numbers {
+            args.push(String::from("-n"));
+        }
+        if only_match_words {
+            args.push(String::from("-w"));
+        }
+        if inverted_match {
+            args.push(String::from("-v"));
+        }
+        
+        args.push(file_path.clone()); // Needs to be last
+
+        let config = Config::build(&args); // This is missing the query
+
+        // Should fail due to the query missing
+        assert!(config.is_err());
+    }
+}
